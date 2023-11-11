545c07cb0f45d631bba38da06f952566d041b1cf
sein yang <did756984@naver.com>
2023-11-11 19:08:11 +0900

    [main]

---
+++ src/main/java/com/example/devlogbackend/DevlogBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.devlogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlogBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevlogBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devlogbackend/DevlogBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devlogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlogBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
